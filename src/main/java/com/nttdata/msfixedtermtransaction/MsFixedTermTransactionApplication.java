a6ee04d8d1a022b08cab2c1486f99ac73b502f76
Mauricio Kevin Sulca Chavez <msulccha@emeal.nttdata.com>
2022-08-18 19:15:51 -0500

    primer

---
+++ src/main/java/com/nttdata/msfixedtermtransaction/MsFixedTermTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.msfixedtermtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFixedTermTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFixedTermTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/msfixedtermtransaction/MsFixedTermTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.msfixedtermtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFixedTermTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
